1c13a9f1429aeeb3026201b23dc4f2d0467fbcad
Denis <lde@mail.ru>
2018-05-25 15:47:24 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import requests  # осуществляет работу с HTTP-запросами
+import urllib.request  # библиотека HTTP
+from lxml import html  # библиотека для обработки разметки xml и html, импортируем только для работы с html
+import re  # осуществляет работу с регулярными выражениями
+from bs4 import BeautifulSoup  # осуществляет синтаксический разбор документов HTML
+import csv  # осуществляет запись файла в формате CSV
+import tkinter  # создание интерфейса
+from tkinter.filedialog import *  # диалоговые окна
